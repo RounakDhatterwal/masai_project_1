3f31899a102160880a3aeba8f7a8f8e222e75214
RounakDhatterwal <rounakdhatterwal43215@gmail.com>
2022-10-19 10:59:42 +0530

    login

---
+++ login.js
@@ -0,0 +1,14 @@
+let reg_email = "abc@gmail.com";
+let reg_pass = "12454";
+
+let enter_email = "avc@gmail.com";
+let enter_pass = "124542";
+
+if (reg_email == enter_email && reg_pass == enter_email)
+{
+console.log("Login Succcesfull");
+}
+else
+{
+console.log("Wrong Credential");
+}
